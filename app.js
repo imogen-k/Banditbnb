81c9ba0da48e46d80899272a904fb0298e88e955
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-08 16:34:35 +0100

    seperated db property schema to property.js and exported as module, db connection is working

--- app.js
+++ app.js
@@ -2,32 +2,18 @@ var express = require('express');
 var app = express();
 var port = 3000
 var path = require('path')
+var Property = require('./property.js');
 
-
-
-const mongoose = require ('mongoose');
-
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://fraser:Zhe3ren3@banditbnb.1nsfl.mongodb.net/BanditBnB?retryWrites=true&w=majority', {useNewUrlParser: true});
-
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function() {
 
 })
 
 
 app.get('/', function (req, res) {
-  import { Property } from './property.js'
-  const villa = new Property({ name: 'Villa', address: '124 cool kids lane', description: 'Cool villa for cool kids', 
-ppn: 234.55, contact: 'coolkidz@cool.com', availability: true });
-  console.log(villa.name);
-
-  villa.save(function (err, villa) {
-    if (err) return console.error(err);
-    console.log(villa.name);
-  });
   app.use(express.static(__dirname + '/images'));
   res.sendFile(path.join(__dirname + '/routes/landingPage.html'))
 });
--- property.js
+++ property.js
@@ -1,4 +1,4 @@
- 
+const mongoose = require('mongoose')
 
 const propertySchema = new mongoose.Schema({
 name: String,
@@ -10,14 +10,17 @@ availability: Boolean
 })
 
 const Property = mongoose.model('Property', propertySchema);
-const villa = new Property({ name: 'Villa', address: '124 cool kids lane', description: 'Cool villa for cool kids', 
-ppn: 234.55, contact: 'coolkidz@cool.com', availability: true });
-  console.log(villa.name);
 
-  villa.save(function (err, villa) {
-    if (err) return console.error(err);
-    console.log(villa.name);
-  });
+module.exports = Property
+// const villa = new Property({
+//   name: 'Villa', address: '124 cool kids lane', description: 'Cool villa for cool kids', 
+// ppn: 234.55, contact: 'coolkidz@cool.com', availability: true });
+//   console.log(villa.name);
 
-  export { Property };
+//   villa.save(function (err, villa) {
+//     if (err) return console.error(err);
+//     console.log(villa.name);
+//   });
+
+//   export { Property };
 

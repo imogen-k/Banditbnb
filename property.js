131fb135232b20f82f5afa060ca540a4181e12bf
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-10 10:41:37 +0100

    fixed bracket issue in property.js

--- property.js
+++ property.js
@@ -12,20 +12,20 @@ class Property {
 //    this.availability = availability
 //    this.date_available_from = date_available_from
 //    this.date_available_to = date_available_to
-  }
+// }
 
-  new() {
-    new Property(
-      name,
-      address,
-      description,
-      ppm,
-      contact,
-      availability,
-      date_available_from,
-      date_available_to
-    );
-  }
+  // new() {
+  //   new Property(
+  //     name,
+  //     address,
+  //     description,
+  //     ppm,
+  //     contact,
+  //     availability,
+  //     date_available_from,
+  //     date_available_to
+  //   );
+  // }
 
   static all(callback) {
     mongoose.connect('mongodb+srv://fraser:Zhe3ren3@banditbnb.1nsfl.mongodb.net/BanditBnB?retryWrites=true&w=majority', {useNewUrlParser: true});
